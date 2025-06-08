9971a7e4a60a9ccdb030e5e1d2e9817a979993f0
blalab <rcidfer@gmail.com>
2025-06-08 15:04:19 -0400

    Remove onboarding from user-home

--- src/components/tank/user-home.tsx
+++ src/components/tank/user-home.tsx
@@ -18,8 +18,6 @@ import { useContext } from 'react';
 import { GlobalContext } from "@/components/tank/global-context"
 import { NavLink } from "react-router-dom";
 
-import Onboarding from "@/onboarding"
-
 interface Portfolio {
   name: string;
   portfolio_id: string;
